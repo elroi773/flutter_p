8570721ed762feb5eeab28cf8472977328f6e0c6
김이레 <165885725+elroi773@users.noreply.github.com>
2026-08-11 14:17:14 +0900

    box layout 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyWidget());
+  runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Image.asset('assets/textimg.jpg')
+      home: Scaffold(
+        appBar: AppBar(title: const Text("앱임", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+        body: const Text("안녕"),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

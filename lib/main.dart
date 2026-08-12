90df20c1f5cc44af73460a024787a56e8a3f9bec
김이레 <165885725+elroi773@users.noreply.github.com>
2026-08-12 12:00:00 +0900

    Update main app layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("앱임", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),

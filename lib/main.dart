9bd983dac9154c52491bea15be7b9957ae230beb
김이레 <165885725+elroi773@users.noreply.github.com>
2026-08-13 10:52:30 +0900

    박스 레이아웃

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("앱임", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
-        body: const Text("안녕"),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page),
-            ],
-          ),
+        body: Container(
+          width: 50,
+          height: 50,
+          margin: EdgeInsets.all(20), //바깥 여백
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(color: Colors.black),
+          ), //Style
+          //margin: EdgeInsets.fromLTRB 왼쪽 위 오른쪽 아래
+          child: Text('dddd'),
         ),
       ),
     );
